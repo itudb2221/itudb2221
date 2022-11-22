6960d79d528b6a41e61c6ff773d0448edf8c4d9c
rojenarda <sesen19@itu.edu.tr>
2022-11-22 19:21:06 +0300

    Created initial project files and templates.

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import views
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("settings")
+
+    app.add_url_rule('/', view_func=views.home_page)
+    app.add_url_rule('/drivers', view_func=views.drivers_page)
+    app.add_url_rule('/seasons', view_func=views.seasons_page)
+    return app
+
+app = create_app()
+port = app.config.get("PORT", 5000)
+app.run(host="0.0.0.0", port=port)
---
+++ views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+
+def home_page():
+    return render_template("home.html")
+
+def drivers_page():
+    return render_template("drivers.html")
+
+def seasons_page():
+    return render_template("seasons.html")
