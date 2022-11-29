dea7948ccf1f7fb15ada8856db4130fe32606588
rojenarda <sesen19@itu.edu.tr>
2022-11-29 21:34:13 +0300

    [UPDATE] Added 'get_drivers' function for querying drivers.

--- database.py
+++ database.py
@@ -4,14 +4,15 @@ import sqlite3 as sqlite
 class Database:
     def __init__(self, dbfile):
         self.dbfile = dbfile
-    
+    # ============== Drivers Start =============== #
     def addDriver(self, driver: Driver):
         with sqlite.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            query = "INSERT INTO DRIVERS (driverRef, driverNumber, code, forename, surname, dob, nationality, driverUrl) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+            query = "INSERT INTO DRIVERS (driverId, driverRef, driverNumber, code, forename, surname, dob, nationality, driverUrl) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
             cursor.execute(
                 query,
                 (
+                    driver.driverId,
                     driver.driverRef,
                     driver.number,
                     driver.code,
@@ -24,7 +25,19 @@ class Database:
             )
             connection.commit()
     
-    
+    def get_drivers(self):
+        drivers = list()
+        with(sqlite.connect(self.dbfile)) as connection:
+            cursor = connection.cursor()
+            query = "SELECT * FROM DRIVERS ORDER BY dob"
+            cursor.execute(query)
+            for driverId, driverRef, number, code, forename, surname, dob, nationality, url in cursor:
+                drivers.append(Driver(driverId, driverRef, number, code, forename, surname, dob, nationality, url))
+        return drivers
+
+# ============== Drivers End =============== #
+
+# ============== Seasons Start =============== #
     def addSeason(self, season: Season):
         with sqlite.connect(self.dbfile) as connection:
             cursor = connection.cursor()
@@ -38,7 +51,9 @@ class Database:
             )
             connection.commit()
         
+# ============== Seasons End =============== #
 
+# ============== Driver Standings Start =============== #
     def addDriverStanding(self, dst: DriverStanding):
         with sqlite.connect(self.dbfile) as connection:
             cursor = connection.cursor()
@@ -55,3 +70,4 @@ class Database:
                 )
             )
             connection.commit()
+# ============== Driver Standings End =============== #
