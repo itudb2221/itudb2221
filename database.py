dc54839aa1b1be5962cc270432aa1624177b548f
rojenarda <sesen19@itu.edu.tr>
2022-12-16 13:04:45 +0300

    [UPDATE] Added CRUD functions for drivers table.

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ class Database:
     def __init__(self, dbfile):
         self.dbfile = dbfile
     # ============== Drivers Start =============== #
-    def addDriver(self, driver: Driver):
+    def addDriver(self, driver: Driver): # Create
         with sqlite.connect(self.dbfile) as connection:
             cursor = connection.cursor()
             query = "INSERT INTO DRIVERS (driverId, driverRef, driverNumber, code, forename, surname, dob, nationality, driverUrl) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
@@ -25,15 +25,44 @@ class Database:
             )
             connection.commit()
     
-    def get_drivers(self):
+    def get_drivers(self): # Read
         drivers = list()
         with(sqlite.connect(self.dbfile)) as connection:
             cursor = connection.cursor()
             query = "SELECT * FROM DRIVERS ORDER BY dob"
             cursor.execute(query)
+            connection.commit()
             for driverId, driverRef, number, code, forename, surname, dob, nationality, url in cursor:
                 drivers.append(Driver(driverId, driverRef, number, code, forename, surname, dob, nationality, url))
         return drivers
+
+    def update_driver(self, driverId, attrNames, attrValues):
+        if "driverId" in attrNames:
+            print("Primary key cannot be updated.") # !!! Display message on screen later.
+            return
+        if (len(attrNames) != len(attrValues)) or not len(attrNames):
+            print("Invalid input. ") # !!! Display message on screen later.
+            return
+
+        with (sqlite.connect(self.dbfile)) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE DRIVERS SET "
+            for i in range(len(attrNames)):
+                query += (f" {attrNames[i]} = {attrValues[i]},")
+            query = query[:-1] + "WHERE driverId = %s"
+            cursor.execute(query, driverId)
+
+
+
+    def remove_driver(self, driverId): # Delete
+        with (sqlite.connect(self.dbfile)) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM DRIVERS WHERE (driverId = %s)"
+            cursor.execute(query, driverId)
+            connection.commit()
+
+
+
 
 # ============== Drivers End =============== #
 
