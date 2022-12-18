fe836858bd652014a49093c12c2a03fceb07a8a6
rojenarda <sesen19@itu.edu.tr>
2022-12-19 00:06:03 +0300

    Race model

--- models.py
+++ models.py
@@ -35,6 +35,27 @@ class Season:
         self.seasonYear = seasonYear
         self.seasonUrl  = seasonUrl
 
+class Race:
+    def __init__(self, raceId, raceYear, raceRound, circutId, raceName, raceDate, raceTime, raceUrl, fp1_date, fp1_time, fp2_date, fp2_time, fp3_date, fp3_time, quali_date, quali_time, sprint_date, sprint_time):
+        self.raceId      = raceId
+        self.raceYear    = raceYear
+        self.raceRound   = raceRound
+        self.circutId    = circutId
+        self.raceName    = raceName
+        self.raceDate    = raceDate
+        self.raceTime    = raceTime
+        self.raceUrl     = raceUrl
+        self.fp1_date    = fp1_date
+        self.fp1_time    = fp1_time
+        self.fp2_date    = fp2_date
+        self.fp2_time    = fp2_time
+        self.fp3_date    = fp3_date
+        self.fp3_time    = fp3_time
+        self.quali_date  = quali_date
+        self.quali_time  = quali_time
+        self.sprint_date = sprint_date
+        self.sprint_time = sprint_time
+
 class DriverStanding:
     def __init__(self, driverStandingsId, raceId, driverId, points, position, positionText, wins):
         self.driverStandingsId  = driverStandingsId
@@ -44,3 +65,5 @@ class DriverStanding:
         self.position           = position
         self.positionText       = positionText
         self.wins               = wins
+
+
