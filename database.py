c4644489d6e282986d3971e16550c27cbb1f9828
rojenarda <sesen19@itu.edu.tr>
2022-12-18 20:14:39 +0300

    Added CRUD methods for table "CONSTRUCTORS"

--- database.py
+++ database.py
@@ -4,23 +4,22 @@ import sqlite3 as sqlite
 class Database:
     def __init__(self, dbfile):
         self.dbfile = dbfile
-    # ============== Drivers Start =============== #
-    def addDriver(self, driver: Driver): # Create
+# ============== Drivers Start =============== #
+    def add_driver(self, driver: Driver): # Create
         with sqlite.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            query = "INSERT INTO DRIVERS (driverId, driverRef, driverNumber, code, forename, surname, dob, nationality, driverUrl) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            query = "INSERT INTO DRIVERS (driverRef, driverNumber, code, forename, surname, dob, nationality, driverUrl) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
             cursor.execute(
                 query,
                 (
-                    driver.driverId,
                     driver.driverRef,
-                    driver.number,
+                    driver.driverNumber,
                     driver.code,
                     driver.forename,
                     driver.surname,
                     driver.dob,
                     driver.nationality,
-                    driver.url
+                    driver.driverUrl
                 )
             )
             connection.commit()
@@ -29,14 +28,14 @@ class Database:
         drivers = list()
         with(sqlite.connect(self.dbfile)) as connection:
             cursor = connection.cursor()
-            query = "SELECT * FROM DRIVERS ORDER BY dob"
+            query = "SELECT * FROM DRIVERS" # ORDER BY dob"
             cursor.execute(query)
             connection.commit()
-            for driverId, driverRef, number, code, forename, surname, dob, nationality, url in cursor:
-                drivers.append(Driver(driverId, driverRef, number, code, forename, surname, dob, nationality, url))
+            for driverId, driverRef, driverNumber, code, forename, surname, dob, nationality, driverUrl in cursor:
+                drivers.append(Driver(driverId, driverRef, driverNumber, code, forename, surname, dob, nationality, driverUrl))
         return drivers
 
-    def update_driver(self, driverId, attrNames, attrValues):
+    def update_driver(self, driverId, attrNames, attrValues): # Update
         if "driverId" in attrNames:
             print("Primary key cannot be updated.") # !!! Display message on screen later.
             return
@@ -61,10 +60,60 @@ class Database:
             cursor.execute(query, driverId)
             connection.commit()
 
-
-
-
 # ============== Drivers End =============== #
+
+# ============== Constructors Start ============== #
+    def add_constructor(self, constructor: Constructor): # Create
+        with sqlite.connect(self.dbfile) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO CONSTRUCTORS (constructorId, constructorRef, constructorName, nationality, constructorUrl) VALUES (?, ?, ?, ?)"
+            cursor.execute(
+                query,
+                (
+                    constructor.constructorRef,
+                    constructor.constructorName,
+                    constructor.nationality,
+                    constructor.constructorUrl
+                )
+            )
+            connection.commit()
+    
+    def get_constructors(self): # Read
+        constructors = list()
+        with(sqlite.connect(self.dbfile)) as connection:
+            cursor = connection.cursor()
+            query = "SELECT * FROM DRIVERS" # ORDER BY dob"
+            cursor.execute(query)
+            connection.commit()
+            for constructorId, constructorRef, constructorName, nationality, constructorUrl in cursor:
+                constructors.append(Constructor(constructorId, constructorRef, constructorName, nationality, constructorUrl))
+        return constructors
+
+    def update_constructor(self, constructorId, attrNames, attrValues): # Update
+        if "constructorId" in attrNames:
+            print("Primary key cannot be updated.") # !!! Display message on screen later.
+            return
+        if (len(attrNames) != len(attrValues)) or not len(attrNames):
+            print("Invalid input. ") # !!! Display message on screen later.
+            return
+
+        with (sqlite.connect(self.dbfile)) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE CONSTRUCTORS SET "
+            for i in range(len(attrNames)):
+                query += (f" {attrNames[i]} = {attrValues[i]},")
+            query = query[:-1] + "WHERE constructorId = %s"
+            cursor.execute(query, constructorId)
+
+
+
+    def remove_driver(self, driverId): # Delete
+        with (sqlite.connect(self.dbfile)) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM DRIVERS WHERE (driverId = %s)"
+            cursor.execute(query, driverId)
+            connection.commit()
+# ============== Constructors End ============== #
 
 # ============== Seasons Start =============== #
     def addSeason(self, season: Season):
